fd583b6f88099d0b66d96db6eb91b489bf64e520
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-09 21:15:39 +0530

    Get Team By Slug

--- src/main/java/com/base/Services/TeamService.java
+++ src/main/java/com/base/Services/TeamService.java
@@ -2,6 +2,8 @@ package com.base.Services;
 
 import com.base.Base;
 import com.base.Exceptions.BaseHttpException;
+import com.base.Exceptions.Http.NotFound;
+import com.base.Exceptions.TeamNotFound;
 import com.base.Http.Request.Request;
 import com.base.Http.Response.Response;
 import com.base.Models.Team;
@@ -33,5 +35,26 @@ public class TeamService {
         Response response = this.base.sendRequest("/teams", Request.METHOD_POST, parameters);
         return (Team) Base.makeModel(Team.class, response.getBody());
     }
+
+    /**
+     * Get Team By Slug.
+     *
+     * @param slug
+     * @return Team
+     * @throws TeamNotFound
+     */
+    public Team getTeam(String slug) throws TeamNotFound, BaseHttpException {
+        try {
+            Response response = this.base.sendRequest("/teams/".concat(slug), Request.METHOD_GET);
+            return (Team) Base.makeModel(Team.class, response.getBody());
+        } catch (NotFound e) {
+            throw new TeamNotFound(slug);
+        }
+    }
+
+
+
+
+
 
 }
