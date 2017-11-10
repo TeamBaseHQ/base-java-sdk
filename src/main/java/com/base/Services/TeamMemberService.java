ce160aef5af9b9dc77ac3615cf8856ac2cf1c389
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-10 15:58:04 +0530

    Team Member Service: Show Team Member

--- src/main/java/com/base/Services/TeamMemberService.java
+++ src/main/java/com/base/Services/TeamMemberService.java
@@ -1,12 +1,28 @@
 package com.base.Services;
 
 import com.base.Base;
+import com.base.Exceptions.BaseHttpException;
+import com.base.Exceptions.Http.NotFound;
+import com.base.Exceptions.TeamNotFound;
+import com.base.Http.Request.Request;
+import com.base.Http.Response.Response;
+import com.base.Models.User;
 
 public class TeamMemberService {
     private Base base;
 
     public TeamMemberService(Base base) {
         this.base = base;
+    }
+
+    public User showTeamMember(String teamSlug, String user_id) throws TeamNotFound, BaseHttpException {
+        try {
+            String URL = "/teams/".concat(teamSlug).concat("/members/").concat(user_id);
+            Response response = this.base.sendRequest(URL, Request.METHOD_GET);
+            return (User) Base.makeModel(User.class, response.getBody());
+        } catch (NotFound e) {
+            throw new TeamNotFound(teamSlug);
+        }
     }
 
 
