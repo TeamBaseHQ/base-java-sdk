129decbb0f48f8069db58d7b4fc3db07e53e149f
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-10 15:31:19 +0530

    Team Member Service: Added Team Member Service

---
+++ src/main/java/com/base/Services/TeamMemberService.java
@@ -0,0 +1,13 @@
+package com.base.Services;
+
+import com.base.Base;
+
+public class TeamMemberService {
+    private Base base;
+
+    public TeamMemberService(Base base) {
+        this.base = base;
+    }
+
+
+}
