9c15c1d38381bf32e4b4422ac32c6b45eaee6bcf
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-10 20:45:57 +0530

    Channel Member Service: Created

---
+++ src/main/java/com/base/Services/ChannelMemberService.java
@@ -0,0 +1,22 @@
+package com.base.Services;
+
+import com.base.Base;
+import com.base.Exceptions.BaseHttpException;
+import com.base.Exceptions.ChannelNotFound;
+import com.base.Exceptions.Http.NotFound;
+import com.base.Http.Request.Request;
+import com.base.Http.Response.Response;
+import com.base.Models.User;
+
+import java.util.*;
+
+public class ChannelMemberService {
+
+    private Base base;
+
+    public ChannelMemberService(Base base) {
+        this.base = base;
+    }
+
+
+}
