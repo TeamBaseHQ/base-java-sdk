b17ea8d9780fae5eb59fb70e7e8ec3ce67d91e5c
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-10 21:44:21 +0530

    Thread Service: Create Channel Thread

---
+++ src/main/java/com/base/Services/ThreadService.java
@@ -0,0 +1,44 @@
+package com.base.Services;
+
+import com.base.Base;
+import com.base.Exceptions.BaseHttpException;
+import com.base.Exceptions.ChannelNotFound;
+import com.base.Exceptions.Http.NotFound;
+import com.base.Http.Request.Request;
+import com.base.Http.Response.Response;
+import com.base.Models.Thread;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ThreadService {
+    private Base base;
+
+    public ThreadService(Base base) {
+        this.base = base;
+    }
+
+    /**
+     * Create Thread Messages
+     *
+     * @param teamSlug    Team Slug Name
+     * @param channelSlug Channel Slug Name
+     * @param subject     Thread subject
+     * @param description Thread description
+     * @return Thread
+     * @throws BaseHttpException Exception
+     * @throws ChannelNotFound   Exception
+     */
+    public Thread createThreadMessage(String teamSlug, String channelSlug, String subject, String description) throws BaseHttpException, ChannelNotFound {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("subject", subject);
+        parameters.put("description", description);
+        try {
+            String URL = "/teams/".concat(teamSlug).concat("/channels/").concat(channelSlug).concat("/threads");
+            Response response = this.base.sendRequest(URL, Request.METHOD_POST, parameters);
+            return (Thread) Base.makeModel(Thread.class, response.getBody());
+        } catch (NotFound e) {
+            throw new ChannelNotFound(channelSlug);
+        }
+    }
+}
