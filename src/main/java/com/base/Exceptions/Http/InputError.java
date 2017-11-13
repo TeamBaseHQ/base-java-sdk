51a0db6a0643eaa351aaf3eb522a2d9ea45dd8fe
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 14:46:25 +0530

    JavaDoc: Added comment to Input Error Class

--- src/main/java/com/base/Exceptions/Http/InputError.java
+++ src/main/java/com/base/Exceptions/Http/InputError.java
@@ -6,16 +6,34 @@ import com.base.Http.Response.Response;
 
 public class InputError extends BaseHttpException {
 
+    /**
+     * ErrorBag
+     */
     protected ErrorBag errorBag;
 
+    /**
+     * Default Constructor
+     * @param code
+     * @param message
+     * @param response
+     */
     public InputError(int code, String message, Response response) {
         super(code, message, response);
     }
 
+    /**
+     * Get ErrorBag
+     * @return ErrorBag
+     */
     public ErrorBag getErrorBag() {
         return errorBag;
     }
 
+    /**
+     * Set ErrorBag
+     * @param errorBag
+     * @return
+     */
     public InputError setErrorBag(ErrorBag errorBag) {
         this.errorBag = errorBag;
         return this;
