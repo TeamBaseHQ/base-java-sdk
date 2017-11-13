5d93721cbd4f90abb5be323fcfde2f1bd6330379
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 15:15:56 +0530

    JavaDoc: Added Comment to BaseHttpException Excetpion Class

--- src/main/java/com/base/Exceptions/BaseHttpException.java
+++ src/main/java/com/base/Exceptions/BaseHttpException.java
@@ -4,17 +4,35 @@ import com.base.Http.Response.Response;
 
 public class BaseHttpException extends BaseException {
 
+    /**
+     * HTTP {@link Response}
+     */
     protected Response response;
 
+    /**
+     * Constructs {@link BaseHttpException} with the specified code,message and response
+     * @param code Status Code
+     * @param message Error Message
+     * @param response Response
+     */
     public BaseHttpException(int code, String message, Response response) {
         super(code, message);
         this.response = response;
     }
 
+    /**
+     * Get the {@link Response}
+     * @return Response
+     */
     public Response getResponse() {
         return response;
     }
 
+    /**
+     * Construct {@link BaseHttpException} with response.
+     * @param response Response
+     * @return
+     */
     public BaseHttpException setResponse(Response response) {
         this.response = response;
         return this;
