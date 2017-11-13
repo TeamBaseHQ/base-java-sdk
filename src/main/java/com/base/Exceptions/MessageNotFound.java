e406c908eed86480fb840a6663510d92ab65e868
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 16:23:06 +0530

    JavaDoc: Message Not Found Comment Added

--- src/main/java/com/base/Exceptions/MessageNotFound.java
+++ src/main/java/com/base/Exceptions/MessageNotFound.java
@@ -1,6 +1,10 @@
 package com.base.Exceptions;
 
 public class MessageNotFound extends BaseException {
+    /**
+     * Construct {@link MessageNotFound} with Message Slug and call the {@link BaseException} with Error Code
+     * @param messageSlug Message Slug of the Message
+     */
     public MessageNotFound(String messageSlug) {
         super(404, "Message with slug " + messageSlug + " was not found.");
     }
