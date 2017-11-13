f9a74a670e26c37e7a24663067aa7d135debc4e0
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 14:47:33 +0530

    JavaDoc: Not Found Class Comment

--- src/main/java/com/base/Exceptions/Http/NotFound.java
+++ src/main/java/com/base/Exceptions/Http/NotFound.java
@@ -5,6 +5,12 @@ import com.base.Http.Response.Response;
 
 public class NotFound extends BaseHttpException {
 
+    /**
+     * Not Found Exception Class
+     * @param code
+     * @param message
+     * @param response
+     */
     public NotFound(int code, String message, Response response) {
         super(code, message, response);
     }
