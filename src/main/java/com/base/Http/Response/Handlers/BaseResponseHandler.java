ab7be33e4c6dc2dc7e97a531f64900c7900bcb16
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 18:58:28 +0530

    JavaDoc: Added Comment to BaseResponseHandler.java

--- src/main/java/com/base/Http/Response/Handlers/BaseResponseHandler.java
+++ src/main/java/com/base/Http/Response/Handlers/BaseResponseHandler.java
@@ -5,22 +5,20 @@ import com.base.Exceptions.Http.InputError;
 import com.base.Exceptions.Http.NotFound;
 import com.base.Http.Response.ErrorBag;
 import com.base.Http.Response.Response;
-import com.base.Models.ResponseModel;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class BaseResponseHandler implements HandlerInterface {
 
-    public Response handle(Response response) throws BaseHttpException {
-        int code = response.getStatusCode();
-
-        if (code >= 400) {
-            BaseResponseHandler.throwException(response);
-        }
-
-        return response;
-    }
-
+    /**
+     * It will take Response as argument.
+     * It will throw {@link NotFound} Exception when Response's StatusCode is equals-to 404
+     * and throw {@link BaseResponseHandler.inputError} Exception if StatusCode is equals-to 422
+     * else it will throw {@link BaseHttpException}
+     *
+     * @param response
+     * @throws BaseHttpException
+     */
     private static void throwException(Response response) throws BaseHttpException {
         int statusCode = response.getStatusCode();
         String message = response.getBody();
@@ -36,6 +34,14 @@ public class BaseResponseHandler implements HandlerInterface {
         throw new BaseHttpException(statusCode, message, response);
     }
 
+    /**
+     * Throw An Exception according to status code
+     *
+     * @param statusCode Request's status code
+     * @param message    Request's Message
+     * @param response   Response
+     * @return BaseHttpException exception
+     */
     private static BaseHttpException inputError(int statusCode, String message, Response response) {
         InputError error = new InputError(statusCode, message, response);
         GsonBuilder builder = new GsonBuilder();
@@ -45,6 +51,23 @@ public class BaseResponseHandler implements HandlerInterface {
         error.setErrorBag(errorBag);
 
         return error;
+    }
+
+    /**
+     * It will throw an exception when Response's StatusCode is grater than or equal to 400.
+     *
+     * @param response {@link Response}
+     * @return Response
+     * @throws BaseHttpException Exception
+     */
+    public Response handle(Response response) throws BaseHttpException {
+        int code = response.getStatusCode();
+
+        if (code >= 400) {
+            BaseResponseHandler.throwException(response);
+        }
+
+        return response;
     }
 
 }
