e081be045eba98cfbb4470b4c71cafa632ec0d40
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 20:29:58 +0530

    JavaDoc: Added Comment to ResponseModel.java

--- src/main/java/com/base/Models/ResponseModel.java
+++ src/main/java/com/base/Models/ResponseModel.java
@@ -2,34 +2,76 @@ package com.base.Models;
 
 public class ResponseModel {
 
+    /**
+     * Response data
+     */
     protected Object data;
 
-    protected  Object links;
+    /**
+     * Response links
+     */
+    protected Object links;
 
-    protected  Object meta;
+    /**
+     * Response meta
+     */
+    protected Object meta;
 
+    /**
+     * Response Mode Meta
+     *
+     * @return Meta
+     */
     public Object getMeta() {
         return meta;
     }
 
+    /**
+     * Response Model Meta
+     *
+     * @param meta
+     * @return
+     */
     public ResponseModel setMeta(Object meta) {
         this.meta = meta;
         return this;
     }
 
+    /**
+     * Response Mode Links
+     *
+     * @return Links
+     */
     public Object getLinks() {
         return links;
     }
 
+    /**
+     * Response Model Links
+     *
+     * @param links
+     * @return
+     */
     public ResponseModel setLinks(Object links) {
         this.links = links;
         return this;
     }
 
+    /**
+     * Response Mode Data
+     *
+     * @return Data
+     */
     public Object getData() {
         return data;
     }
 
+    /**
+     * Response Model Data
+     *
+     * @param data
+     * @return
+     */
     public ResponseModel setData(Object data) {
         this.data = data;
         return this;
