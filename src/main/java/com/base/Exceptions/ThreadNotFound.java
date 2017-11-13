0c95e9512b736462988935363b46673660a0e7e1
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 16:23:50 +0530

    JavaDoc: Thread Not Found Comment Added

--- src/main/java/com/base/Exceptions/ThreadNotFound.java
+++ src/main/java/com/base/Exceptions/ThreadNotFound.java
@@ -1,6 +1,11 @@
 package com.base.Exceptions;
 
 public class ThreadNotFound extends BaseException {
+    /**
+     * Construct {@link ThreadNotFound} with Thread Slug and call the {@link BaseException} with Error Code
+     *
+     * @param threadSlug Thread Slug of Thread
+     */
     public ThreadNotFound(String threadSlug) {
         super(404, "Thread with Slug " + threadSlug + " was not found.");
     }
