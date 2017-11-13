764b13bb0b488acad9e291f6fd4a421db261fe28
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 18:41:07 +0530

    JavaDoc: Added Comment to JSONRequestBody.java

--- src/main/java/com/base/Http/Request/JSONRequestBody.java
+++ src/main/java/com/base/Http/Request/JSONRequestBody.java
@@ -2,12 +2,25 @@ package com.base.Http.Request;
 
 public class JSONRequestBody implements RequestBody {
 
+    /**
+     * JSON Encoded Body
+     */
     protected String encodedBody;
 
+    /**
+     * Request Request Body
+     *
+     * @param data
+     */
     public JSONRequestBody(Object data) {
         this.encodedBody = data.toString();
     }
 
+    /**
+     * Return Body of Request
+     *
+     * @return Body
+     */
     public String getBody() {
         return this.encodedBody;
     }
