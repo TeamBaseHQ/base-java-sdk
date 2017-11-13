01d3ebe848db5aa2b6f24814276a95e96cdf759b
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-13 19:21:32 +0530

    JavaDoc: Added Comment to Channel.java

--- src/main/java/com/base/Models/Channel.java
+++ src/main/java/com/base/Models/Channel.java
@@ -31,58 +31,128 @@ public class Channel extends BaseModel {
      */
     private String slug;
 
+    /**
+     * Return Channel Name
+     *
+     * @return Name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set Channel Name
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Return Channel Description
+     *
+     * @return Description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set Channel Description
+     *
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Return Channel Color
+     *
+     * @return Color
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Set Channel Color
+     *
+     * @param color
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * Return Channel Type
+     *
+     * @return Type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Set Channel Type
+     *
+     * @param type
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Return Channel User_id
+     *
+     * @return User_id
+     */
     public Integer getUser_id() {
         return user_id;
     }
 
+    /**
+     * Set Channel User_id
+     *
+     * @param user_id
+     */
     public void setUser_id(Integer user_id) {
         this.user_id = user_id;
     }
 
+    /**
+     * Return Channel Team_id
+     *
+     * @return Team_id
+     */
     public Integer getTeam_id() {
         return team_id;
     }
 
+    /**
+     * Set Channel Team_id
+     *
+     * @param team_id
+     */
     public void setTeam_id(Integer team_id) {
         this.team_id = team_id;
     }
 
+    /**
+     * Return Channel Slug
+     *
+     * @return Slug
+     */
     public String getSlug() {
         return slug;
     }
 
+    /**
+     * Set Channel Slug
+     *
+     * @param slug
+     */
     public void setSlug(String slug) {
         this.slug = slug;
     }
