b2551bbf8b4bdd259ef4b6ff05790a92153cd41d
Kunal Varma <kunalvarma05@gmail.com>
2017-11-15 06:29:26 +0530

    Fix Helper

--- src/main/java/com/base/Helpers.java
+++ src/main/java/com/base/Helpers.java
@@ -1,8 +1,38 @@
 package com.base;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Helpers {
+
+    /**
+     * Join List Elements with a String.
+     *
+     * @param glue String to glue list elements.
+     * @param list List of elements.
+     * @param <T>  List element type.
+     * @return String
+     */
+    public static <T> String implode(String glue, List<T> list) {
+        // list is empty, return empty string
+        if (list == null || list.isEmpty()) {
+            return "";
+        }
+
+        Iterator<T> iter = list.iterator();
+
+        // init the builder with the first element
+        StringBuilder sb = new StringBuilder();
+        sb.append(iter.next());
+
+        // concat each element
+        while (iter.hasNext()) {
+            sb.append(glue).append(iter.next());
+        }
+
+        // return result
+        return sb.toString();
+    }
 
     /**
      * Build URL with Query
@@ -12,6 +42,6 @@ public class Helpers {
      * @return URL
      */
     public static String buildUrlWithQuery(String url, List<String> parameters) {
-        return url.concat("?").concat(String.join("&", parameters));
+        return url.concat("?").concat(Helpers.implode("&", parameters));
     }
 }
