78d86f4a0f68563e8b4db3ce78970c9605059e3c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-18 03:35:19 +0530

    AbstractBaseTest: Test setup and bootstrap

--- src/test/java/com/base/AbstractBaseTest.java
+++ src/test/java/com/base/AbstractBaseTest.java
@@ -1,32 +1,54 @@
 package com.base;
 
-import com.base.Auth.AccessToken;
-import com.base.Exceptions.BaseException;
 import com.base.Http.Clients.TestHttpClient;
 import com.base.Http.Server.TestHttpServer;
-import junit.framework.TestCase;
-import org.junit.BeforeClass;
+import org.junit.Before;
 
-public abstract class AbstractBaseTest extends TestCase {
+public abstract class AbstractBaseTest {
 
-    protected static void getUserAccessToken(Base base) {
-        try {
-            AccessToken accessToken = base.getUserAccessToken("kunalvarma05@gmail.com", "abcd1234");
-            base.getClient().setAccessToken(accessToken);
-        } catch (BaseException e) {
-            e.printStackTrace();
-        }
+    protected Base base;
+
+
+    /**
+     * Get Base instance.
+     *
+     * @return Base Client Instance.
+     */
+    public Base getBase() {
+        return base;
     }
 
+    /**
+     * Bootstrap.
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.base = setupBase();
+    }
+
+    /**
+     * Setup base.
+     *
+     * @return Base Client
+     */
     protected static Base setupBase() {
         TestHttpServer server = AbstractBaseTest.makeServer();
         TestHttpClient httpClient = new TestHttpClient(server);
 
         BaseClient baseClient = new BaseClient(httpClient);
-        baseClient.setClientId("1").setClientSecret("eyIzeSButivrkHGc1PAfU4IBclYONeRy73eZvi1b");
+        baseClient
+                .setApiUrl("localhost")
+                .setClientId("1")
+                .setClientSecret("secret");
+
         return new Base(baseClient);
     }
 
+    /**
+     * Make a Test API Server.
+     *
+     * @return TestHttpServer Instance.
+     */
     protected static TestHttpServer makeServer() {
         RouteMappings routeMappings = new RouteMappings();
         return new TestHttpServer(routeMappings.getRoutes());
