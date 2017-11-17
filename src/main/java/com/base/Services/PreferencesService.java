ed635e0d74ca06392acbba58c787713522c7c2e3
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-17 19:35:06 +0530

    Preferences: Added Preferences Service Model

---
+++ src/main/java/com/base/Services/PreferencesService.java
@@ -0,0 +1,22 @@
+package com.base.Services;
+
+import com.base.Base;
+
+public class PreferencesService {
+
+    /**
+     * {@link Base}
+     */
+    private Base base;
+
+    /**
+     * Construct Instance of Base Class
+     *
+     * @param base
+     */
+    public PreferencesService(Base base) {
+        this.base = base;
+    }
+
+
+}
