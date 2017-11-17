c762e86c9caa369ce229ed123a16d37e4bb69e87
Kunal Varma <kunalvarma05@gmail.com>
2017-11-18 03:39:20 +0530

    TestHttpServer: Make and dispatch request

--- src/test/java/com/base/Http/Server/TestHttpServer.java
+++ src/test/java/com/base/Http/Server/TestHttpServer.java
@@ -10,27 +10,59 @@ import java.util.Map;
 
 public class TestHttpServer {
 
+    /**
+     * Route Mappings.
+     */
     private static Map<String, ServerResponseInterface> routeMappings;
 
+    /**
+     * Create new TestHttpServer.
+     */
     public TestHttpServer() {
         TestHttpServer.routeMappings = new HashMap<>();
     }
 
+    /**
+     * Create new TestHttpServer with Route Mappings.
+     *
+     * @param mappings Route Mappings.
+     */
     public TestHttpServer(Map<String, ServerResponseInterface> mappings) {
         TestHttpServer.routeMappings = mappings;
     }
 
+    /**
+     * Handle the Request.
+     *
+     * @param request  Request to Handle.
+     * @param response Pre-instantiated Response.
+     * @return Server Response.
+     */
     public Response handle(Request request, Response response) {
-        ServerResponseInterface responseFromServer = TestHttpServer.getServerResponse(request.getMethod(), request.getUrl());
+        // Fetch (Dummy) Response from Server.
+        ServerResponseInterface responseFromServer =
+                TestHttpServer.getServerResponse(
+                        request.getMethod(),
+                        request.getUrl()
+                );
 
+        // No Response was fetched.
         if (responseFromServer == null) {
-           ServerResponseInterface res = new NotFoundResponse();
-           return res.getResponse(request, response);
+            ServerResponseInterface res = new NotFoundResponse();
+            return res.getResponse(request, response);
         }
 
+        // Get Response.
         return responseFromServer.getResponse(request, response);
     }
 
+    /**
+     * Fetch Response for the given method-url combo.
+     *
+     * @param method Request Method. (GET|PUT|PATCH|POST|UPDATE|DELETE)
+     * @param url Request URL.
+     * @return (Dummy) Server Response.
+     */
     private static ServerResponseInterface getServerResponse(String method, String url) {
         String signature = method.concat(" ").concat(url);
         return TestHttpServer.routeMappings.getOrDefault(signature, null);
