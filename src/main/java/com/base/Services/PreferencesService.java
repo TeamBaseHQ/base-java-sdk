bc1e1d8e26e58ba5e55494da9df18a1617a221bc
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-17 19:57:12 +0530

    Preferences: Added Save Preferences

--- src/main/java/com/base/Services/PreferencesService.java
+++ src/main/java/com/base/Services/PreferencesService.java
@@ -1,6 +1,12 @@
 package com.base.Services;
 
 import com.base.Base;
+import com.base.Exceptions.BaseHttpException;
+import com.base.Http.Request.Request;
+import com.base.Http.Response.Response;
+import com.base.Models.Preferences;
+
+import java.util.HashMap;
 
 public class PreferencesService {
 
@@ -18,5 +24,24 @@ public class PreferencesService {
         this.base = base;
     }
 
+
+    /**
+     * Save New Preference of User
+     *
+     * @param value
+     * @param category
+     * @param name
+     * @return Preference
+     * @throws BaseHttpException Exception
+     */
+    public Preferences createPreferences(String name, String value, String category) throws BaseHttpException {
+        HashMap<String, String> parameters = new HashMap<>();
+        parameters.put("value", value);
+        parameters.put("category", category);
+        parameters.put("name", name);
+        String URL = "/preferences/".concat(name);
+        Response response = this.base.sendRequest(URL, Request.METHOD_PUT, parameters);
+        return (Preferences) Base.makeModel(Preferences.class, response.getBody());
+    }
 
 }
