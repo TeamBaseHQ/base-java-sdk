b4f240ccd4e332ed72a1f7afcd856be6c5c991ce
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-18 16:54:56 +0530

    Added Test Case for Get all Channel Service Test

--- src/test/java/com/base/Services/ChannelServiceTest.java
+++ src/test/java/com/base/Services/ChannelServiceTest.java
@@ -8,12 +8,13 @@ import com.base.Exceptions.TeamNotFound;
 import com.base.Http.Request.Request;
 import com.base.Http.Response.Response;
 import com.base.Http.Server.Responses.Channel.CreateChannelResponse;
+import com.base.Http.Server.Responses.Channel.GetAllChannelsResponse;
 import com.base.Http.Server.Responses.Channel.GetChannelResponse;
 import com.base.Models.Channel;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -51,11 +52,20 @@ public class ChannelServiceTest extends AbstractBaseTest {
     @Test
     public void getAllChannels() throws BaseHttpException, TeamNotFound {
 
-        List<String> expectedChannel = Arrays.asList("Development", "design channel", "design channel", "design channel", "design channel");
-        List<Channel> ActualChannel = base.channelService().getAllChannels("twitter-1-2");
+        List<Channel> channels = new ArrayList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            channels.add(new Channel().setColor(GetAllChannelsResponse.VALID_COLOR)
+                    .setName(GetAllChannelsResponse.VALID_NAME.concat(" " + i))
+                    .setDescription(GetAllChannelsResponse.VALID_DESCRIPTION)
+                    .setIs_private(GetAllChannelsResponse.VALID_STATUS)
+                    .setSlug(GetAllChannelsResponse.VALID_CHANNEL_SLUG.concat("-" + i)));
+        }
+
+        List<Channel> ActualChannel = base.channelService().getAllChannels(GetAllChannelsResponse.VALID_TEAM_SLUG);
         for (int i = 0; i < ActualChannel.size(); i++) {
             String actualName = ActualChannel.get(i).getName();
-            String expectName = expectedChannel.get(i);
+            String expectName = channels.get(i).getName();
             Assert.assertEquals(actualName, expectName);
         }
     }
