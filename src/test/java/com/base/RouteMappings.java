bf9cc248a9eac982a232892926b2d6770bb1b77f
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-18 17:07:34 +0530

    Get Update Chaannel Service Test Added in RouteMapping

--- src/test/java/com/base/RouteMappings.java
+++ src/test/java/com/base/RouteMappings.java
@@ -3,6 +3,7 @@ package com.base;
 import com.base.Http.Server.Responses.Channel.CreateChannelResponse;
 import com.base.Http.Server.Responses.Channel.GetAllChannelsResponse;
 import com.base.Http.Server.Responses.Channel.GetChannelResponse;
+import com.base.Http.Server.Responses.Channel.UpdateChannelResponse;
 import com.base.Http.Server.Responses.ServerResponseInterface;
 import com.base.Http.Server.Responses.User.GetUserResponse;
 import com.base.Http.Server.Responses.User.UserLoginResponse;
@@ -16,6 +17,7 @@ public class RouteMappings {
 
     public RouteMappings() {
         registerUserMappings();
+        registerChannelMappings();
     }
 
     private void registerUserMappings() {
@@ -23,11 +25,16 @@ public class RouteMappings {
         routes.put("GET localhost/users/" + String.valueOf(GetUserResponse.VALID_USER_ID),
                 new GetUserResponse()
         );
+    }
+
+    private void registerChannelMappings() {
         routes.put("POST localhost/teams/".concat(CreateChannelResponse.VALID_TEAM_SLUG).concat("/channels"),
                 new CreateChannelResponse());
         routes.put("GET localhost/teams/".concat(GetChannelResponse.VALID_TEAM_SLUG).concat("/channels/").concat(GetChannelResponse.VALID_CHANNEL_SLUG),
                 new GetChannelResponse());
         routes.put("GET localhost/teams/".concat(GetAllChannelsResponse.VALID_TEAM_SLUG).concat("/channels"), new GetAllChannelsResponse());
+        routes.put("POST localhost/teams/".concat(UpdateChannelResponse.VALID_TEAM_SLUG).concat("/channels/".concat(UpdateChannelResponse.VALID_CHANNEL_SLUG)),
+                new UpdateChannelResponse());
     }
 
     public Map<String, ServerResponseInterface> getRoutes() {
