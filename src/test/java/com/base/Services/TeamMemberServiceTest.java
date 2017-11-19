4850b3622658c9335e19b42ae494875c62e25895
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-19 17:59:35 +0530

    Add Test case for Get All Members of team

--- src/test/java/com/base/Services/TeamMemberServiceTest.java
+++ src/test/java/com/base/Services/TeamMemberServiceTest.java
@@ -4,10 +4,14 @@ import com.base.AbstractBaseTest;
 import com.base.Exceptions.BaseHttpException;
 import com.base.Exceptions.TeamNotFound;
 import com.base.Http.Server.Responses.TeamMember.CreateTeamMemberResponse;
+import com.base.Http.Server.Responses.TeamMember.GetAllTeamMembersResponse;
 import com.base.Http.Server.Responses.TeamMember.GetTeamMemberResponse;
 import com.base.Models.User;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamMemberServiceTest extends AbstractBaseTest {
 
@@ -40,5 +44,22 @@ public class TeamMemberServiceTest extends AbstractBaseTest {
         }
     }
 
+    @Test
+    public void testGetAllTeams() throws BaseHttpException, TeamNotFound {
 
+        List<User> users = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            users.add((User) new User()
+                    .setEmail(GetAllTeamMembersResponse.VALID_USER_NAME + i + "@gmail.com")
+                    .setName(GetAllTeamMembersResponse.VALID_USER_NAME + "i")
+                    .setId(GetAllTeamMembersResponse.VALID_USER_ID + i));
+        }
+
+        List<User> ActualTeam = base.teamMemberService().getAllTeamMembers(GetAllTeamMembersResponse.VALID_TEAM_SLUG);
+        for (int i = 0; i < ActualTeam.size(); i++) {
+            String actualName = ActualTeam.get(i).getEmail();
+            String expectName = users.get(i).getEmail();
+            Assert.assertEquals(actualName, expectName);
+        }
+    }
 }
