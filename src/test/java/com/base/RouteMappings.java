cc2c9af60693855b96f77bb85be68e652f7b2c7d
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-19 17:59:21 +0530

    Added Routing for Get All Team Members

--- src/test/java/com/base/RouteMappings.java
+++ src/test/java/com/base/RouteMappings.java
@@ -4,6 +4,7 @@ import com.base.Http.Server.Responses.Channel.*;
 import com.base.Http.Server.Responses.ServerResponseInterface;
 import com.base.Http.Server.Responses.Team.*;
 import com.base.Http.Server.Responses.TeamMember.CreateTeamMemberResponse;
+import com.base.Http.Server.Responses.TeamMember.GetAllTeamMembersResponse;
 import com.base.Http.Server.Responses.TeamMember.GetTeamMemberResponse;
 import com.base.Http.Server.Responses.User.GetUserResponse;
 import com.base.Http.Server.Responses.User.UserLoginResponse;
@@ -26,6 +27,7 @@ public class RouteMappings {
         routes.put("POST localhost/teams/".concat(CreateTeamMemberResponse.VALID_SLUG).concat("/members"), new CreateTeamMemberResponse());
         routes.put("GET localhost/teams/".concat(GetTeamMemberResponse.VALID_TEAM_SLUG).concat("/members/").concat(GetTeamMemberResponse.VALID_USER_ID + ""),
                 new GetTeamMemberResponse());
+        routes.put("GET localhost/teams/".concat(GetAllTeamMembersResponse.VALID_TEAM_SLUG).concat("/members"), new GetAllTeamMembersResponse());
     }
 
     private void registerTeamMapping() {
