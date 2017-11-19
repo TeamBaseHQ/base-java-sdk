2e9c4f9b3cdb1de997f07edb3c9b4e43a630baf5
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-19 14:44:52 +0530

    Add Test case for Get Member of team

--- src/test/java/com/base/Services/TeamMemberServiceTest.java
+++ src/test/java/com/base/Services/TeamMemberServiceTest.java
@@ -4,6 +4,8 @@ import com.base.AbstractBaseTest;
 import com.base.Exceptions.BaseHttpException;
 import com.base.Exceptions.TeamNotFound;
 import com.base.Http.Server.Responses.TeamMember.CreateTeamMemberResponse;
+import com.base.Http.Server.Responses.TeamMember.GetTeamMemberResponse;
+import com.base.Models.User;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,9 +17,28 @@ public class TeamMemberServiceTest extends AbstractBaseTest {
      * @throws BaseHttpException
      */
     @Test
-    public void testTeamMemberCreate() throws BaseHttpException, TeamNotFound {
-        Boolean result = this.base.teamMemberService().addTeamMember(CreateTeamMemberResponse.VALID_SLUG, CreateTeamMemberResponse.VALID_USER_ID);
+    public void testTeamMemberCreate() {
+        Boolean result = null;
+        try {
+            result = this.base.teamMemberService().addTeamMember(CreateTeamMemberResponse.VALID_SLUG, CreateTeamMemberResponse.VALID_USER_ID);
+        } catch (BaseHttpException | TeamNotFound e) {
+            Assert.fail(e.getMessage());
+        }
         System.out.println(result);
         Assert.assertEquals(result, true);
     }
+
+    @Test
+    public void testGetTeamMember() {
+        try {
+            User user = base.teamMemberService().getTeamMember(GetTeamMemberResponse.VALID_TEAM_SLUG, String.valueOf(GetTeamMemberResponse.VALID_USER_ID));
+            Assert.assertEquals(user.getName(), GetTeamMemberResponse.VALID_USER_NAME);
+            Assert.assertEquals(user.getEmail(), GetTeamMemberResponse.VALID_USER_EMAIL);
+            Assert.assertEquals(user.getId(), GetTeamMemberResponse.VALID_USER_ID);
+        } catch (TeamNotFound | BaseHttpException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+
 }
