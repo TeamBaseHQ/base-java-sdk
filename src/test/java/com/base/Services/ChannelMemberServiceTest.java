348fd48ea53296b34135f0123ccd8ab0fd88f972
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-21 18:22:21 +0530

    Added Test case for get member Service Test

--- src/test/java/com/base/Services/ChannelMemberServiceTest.java
+++ src/test/java/com/base/Services/ChannelMemberServiceTest.java
@@ -4,6 +4,8 @@ import com.base.AbstractBaseTest;
 import com.base.Exceptions.BaseHttpException;
 import com.base.Exceptions.ChannelNotFound;
 import com.base.Http.Server.Responses.ChannelMember.CreateChannelMemberResponse;
+import com.base.Http.Server.Responses.ChannelMember.GetChannelMemberResponse;
+import com.base.Models.User;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,7 +20,7 @@ public class ChannelMemberServiceTest extends AbstractBaseTest {
     public void testCreateChannelMember() {
         try {
             boolean result = this.base.channelMemberService()
-                            .addChannelMember(CreateChannelMemberResponse.VALID_TEAM_SLUG,
+                    .addChannelMember(CreateChannelMemberResponse.VALID_TEAM_SLUG,
                             CreateChannelMemberResponse.VALID_CHANNEL_SLUG,
                             CreateChannelMemberResponse.VALID_USER_ID);
             Assert.assertEquals(true, result);
@@ -26,4 +28,21 @@ public class ChannelMemberServiceTest extends AbstractBaseTest {
             Assert.fail(e.getMessage());
         }
     }
+
+    @Test
+    public void testGetChannelMember() {
+        try {
+            User user = base.channelMemberService()
+                    .getChannelMember(GetChannelMemberResponse.VALID_TEAM_SLUG,
+                            GetChannelMemberResponse.VALID_CHANNEL_SLUG,
+                            String.valueOf(GetChannelMemberResponse.VALID_USER_ID));
+            Assert.assertEquals(user.getName(), GetChannelMemberResponse.VALID_USER_NAME);
+            Assert.assertEquals(user.getEmail(), GetChannelMemberResponse.VALID_USER_EMAIL);
+            Assert.assertEquals(user.getId(), GetChannelMemberResponse.VALID_USER_ID);
+        } catch (ChannelNotFound | BaseHttpException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+
 }
