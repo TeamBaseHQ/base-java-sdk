d204ebb144e2c3df6830213ed1939854063fbed7
sharvil1996 <sharvilshah1996@gmail.com>
2017-11-22 11:35:36 +0530

    Added Test case for Thread Create Service

--- src/test/java/com/base/Services/ThreadServiceTest.java
+++ src/test/java/com/base/Services/ThreadServiceTest.java
@@ -1,16 +1,12 @@
 package com.base.Services;
 
 import com.base.AbstractBaseTest;
-import com.base.Base;
 import com.base.Exceptions.BaseHttpException;
-import com.base.Http.Request.Request;
-import com.base.Http.Response.Response;
+import com.base.Exceptions.ChannelNotFound;
 import com.base.Http.Server.Responses.Thread.CreateChannelThreadResponse;
 import com.base.Models.Thread;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.HashMap;
 
 public class ThreadServiceTest extends AbstractBaseTest {
 
@@ -21,21 +17,15 @@ public class ThreadServiceTest extends AbstractBaseTest {
      */
     @Test
     public void testTeamCreate() {
-        HashMap<String, String> parameters = new HashMap<>();
-        parameters.put("subject", CreateChannelThreadResponse.VALID_SUBJECT);
-        parameters.put("description", CreateChannelThreadResponse.VALID_DESCRIPTION);
-        String URL = "/teams/".concat(CreateChannelThreadResponse.VALID_TEAM_SLUG)
-                .concat("/channels/")
-                .concat(CreateChannelThreadResponse.VALID_CHANNEL_SLUG)
-                .concat("/threads");
-
         try {
-            Response response = this.base.sendRequest(URL, Request.METHOD_POST, parameters);
-            Thread thread = (Thread) Base.makeModel(Thread.class, response.getBody());
+            Thread thread = this.base.threadService().addChannelThread(CreateChannelThreadResponse.VALID_TEAM_SLUG,
+                    CreateChannelThreadResponse.VALID_CHANNEL_SLUG,
+                    CreateChannelThreadResponse.VALID_SUBJECT,
+                    CreateChannelThreadResponse.VALID_DESCRIPTION);
             Assert.assertEquals(thread.getSubject(), CreateChannelThreadResponse.VALID_SUBJECT);
             Assert.assertEquals(thread.getDescription(), CreateChannelThreadResponse.VALID_DESCRIPTION);
             Assert.assertEquals(thread.getId(), CreateChannelThreadResponse.VALID_ID);
-        } catch (BaseHttpException e) {
+        } catch (BaseHttpException | ChannelNotFound e) {
             Assert.fail(e.getMessage());
         }
 
